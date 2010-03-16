4de4a95515753337f14511195a481e0a68632084
AB <belozerov.sasha@gmail.com>
2010-03-17 00:53:48 +0600

    Added .gitignore, modified redirect_to urls in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 			if user.login.downcase == @login.downcase && user.password.downcase == @password.downcase
 				session[:id] = user.id
 				flash[:notice] = "Welcome, #{User.find(session[:id]).name}!"
-				redirect_to users_url and return
+				redirect_to user_url(user) and return
 			end
 		end
 	flash[:error] = "Wrong login and/or password"
--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ map.logout 'logout', :controller => 'sessions', :action => 'destroy'
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "users"
+  map.root :controller => "sessions", :action => "new"
 
   # See how all your routes lay out with "rake routes"
 
