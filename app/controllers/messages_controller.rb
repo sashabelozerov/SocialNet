9e1275fb985e867147c20479a96bf31cecbee0c7
AB <belozerov.sasha@gmail.com>
2010-03-21 15:39:16 +0600

    Now users can send messages to each other

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
   def index 
 	@mailbox = params[:mailbox]
 	if @mailbox == "sent"
-     		 @messages = @current_user.messages_as_author.find(:all)
+		 @messages = @current_user.messages_as_author.find(:all)
    	else
       	 @messages = @current_user.messages_as_recipient.find(:all)
    	end
@@ -22,18 +22,20 @@ class MessagesController < ApplicationController
   def create
 	
      @message = @current_user.messages_as_author.build(params[:message])
+	 @message.messageable = User.find_by_login(params[:message][:messageable_id].strip)
+     @message.author = @current_user
      if @message.save
      	 flash[:notice] = "Successfully created message."
-   	 redirect_to user_url(@current_user)
+		redirect_to user_url(@current_user)
      else
        render :action => 'new'
     end
   end
   
   def destroy
-    #@message = Message.find(params[:id])
-    # @message.destroy
-    # flash[:notice] = "Successfully destroyed message."
-    redirect_to messages_url
+    @message = Message.find(params[:id])
+    @message.destroy
+    flash[:notice] = "Successfully destroyed message."
+    redirect_to user_url(@current_user)
   end
 end
