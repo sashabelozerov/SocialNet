1821420c7ab4ceca9c94eeb5c92117bef9e29a9a
AB <belozerov.sasha@gmail.com>
2010-04-02 01:11:01 +0700

    Added possibility to post comments to events (work in progress)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,47 +1,65 @@
 class CommentsController < ApplicationController
+
+  before_filter :find_commentable
+  before_filter :current_user, :only => [:index, :show, :new, :create, :edit, :destroy]
+
   def index
-  	@commentable = ...
+  	@commentable = find_commentable
     @comments = @commentable.comments.all
   end
   
   def show
-  	@commentable = ...
+  	@commentable = find_commentable
     @comment = @commentable.comments.find(params[:id])
   end
   
   def new
-    @comment = Comment.new
+    @comment = @commentable.comments.new
   end
   
   def create
-    @comment = Comment.new(params[:comment])
+    @comment = @commentable.comments.build(params[:comment])
     if @comment.save
       flash[:notice] = "Successfully created comment."
-      redirect_to @comment
+      redirect_to user_url(@current_user)
+      #redirect_to :id => nil
     else
       render :action => 'new'
     end
   end
   
   def edit
-    @comment = Comment.find(params[:id])
+    @comment = @commentable.comments.find(params[:id])
   end
   
   def update
-    @comment = Comment.find(params[:id])
+    @comment = @commentable.comments.find(params[:id])
     if @comment.update_attributes(params[:comment])
       flash[:notice] = "Successfully updated comment."
-      redirect_to @comment
+      redirect_to :id => nil
     else
       render :action => 'edit'
     end
   end
   
   def destroy
-    @comment = Comment.find(params[:id])
+    @comment = @commentable.comments.find(params[:id])
     @comment.destroy
     flash[:notice] = "Successfully destroyed comment."
-    redirect_to comments_url
+    redirect_to :id => nil
   end
+
+  private
+
+  def find_commentable
+    params.each do |name, value|
+      if name =~ /(.+)_id$/
+        @commentable = $1.classify.constantize.find(value)
+        break
+      end
+    end
+    @commentable
+  end
+
 
 end
